b406159d7b79c6db5cdb34472401d95955113576
Dat Chung <datchung@users.noreply.github.com>
2019-12-17 22:30:37 -0500

    Add activities

---
+++ app/src/main/java/com/innochi/audiorecorderbookmark/RecordingActivity.java
@@ -0,0 +1,14 @@
+package com.innochi.audiorecorderbookmark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecordingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recording);
+    }
+}
